803d2d0b53f529056848f34b29d762e93c392f20
sasha <sasha@Mac.lan>
2026-02-27 13:06:59 +0400

    Deploy: 20260227-1306

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pdf-resizer/",
+  base: process.env.TAURI_BUILD ? "/" : "/pdf-resizer/",
   plugins: [react(), svgr()],
   publicDir: "./public",
   build: {
